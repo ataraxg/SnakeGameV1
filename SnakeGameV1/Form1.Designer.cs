4c962aba27a08eb05012ca2984ba09f8144c1bcd
ataraxg <camgene92500@msn.com>
2016-03-07 18:24:57 +0100

    Initial Commit

---
+++ SnakeGameV1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows.Forms;
+
+namespace SnakeGameV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(12, 146);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Timer timer1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
+
---
+++ SnakeGameV1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGameV1
+{
+    public partial class Form1 : Form
+    {
+        private int time = 0;
+        private bool running = false;
+        private int direction = 0;
+        int snakeX;
+        int snakeY;
+        PictureBox snake = new PictureBox();
+        PictureBox[,] pictureBox1 = new PictureBox[38, 20];
+        class Coordinate { int x; int y;
+            public Coordinate(int x, int y) { this.x = x; this.y = y; } }
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(MainForm_KeyDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetFeatureToAllControls(this.Controls);
+            //
+            // PictureBox
+            //
+            
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < 38; i++)
+            {
+                y = 0;
+                for (int j = 0; j < 20; j++) {
+
+                    pictureBox1[i,j] = new PictureBox();
+                    pictureBox1[i,j].Location = new System.Drawing.Point(x, y);
+                    pictureBox1[i,j].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\grass-background-5 - Copie.jpg";
+                    pictureBox1[i,j].Size = new System.Drawing.Size(20, 20);
+                    pictureBox1[i, j].Name = "field";
+
+                    y = y + 20;
+
+                    panel1.Controls.Add(pictureBox1[i,j]);
+                }
+
+                x = x + 20;
+            }
+            
+            pictureBox1[17, 10].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\snake.jpg";
+            snakeX = 17;
+            snakeY = 10;
+
+            // Fruit / Wall
+
+
+            List<Coordinate> listFruit = new List<Coordinate>();
+            List<Coordinate> listWall = new List<Coordinate>();
+            Random rnd = new Random();
+
+            int fruitX = rnd.Next(0, 37);
+            int fruitY = rnd.Next(0, 19);
+
+            pictureBox1[fruitX, fruitY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\cherry.png";
+
+            listFruit.Add(new Coordinate(fruitX, fruitY));
+
+            int wallX = rnd.Next(0, 37);
+            int wallY = rnd.Next(0, 19);
+
+            if (listFruit.Contains(new Coordinate(wallX, wallY)))
+            {
+                wallX = rnd.Next(0, 37);
+                wallY = rnd.Next(0, 19);
+            }
+            else
+            {
+                pictureBox1[wallX, wallY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\wall.png";
+
+                listWall.Add(new Coordinate(wallX, wallY));
+            }
+
+        }
+        private void SetFeatureToAllControls(Control.ControlCollection cc)
+        {
+            if (cc != null)
+            {
+                foreach (Control control in cc)
+                {
+                    control.PreviewKeyDown += new PreviewKeyDownEventHandler(control_PreviewKeyDown);
+                    SetFeatureToAllControls(control.Controls);
+                }
+            }
+        }
+
+        void control_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                e.IsInputKey = true;
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                direction = 0;
+                label2.Text = "Left";
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                direction = 1;
+                label2.Text = "Right";
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                direction = 2;
+                label2.Text = "Up";
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                direction = 3;
+                label2.Text = "Down";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            if (running == false)
+            {
+                button.Text = "Stop";
+                timer1.Enabled = true;
+                timer1.Interval = 100;
+                timer1.Start();
+                running = true;
+            }
+            else {
+                button.Text = "Start";
+                timer1.Stop();
+                running = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (direction == 0)
+            {
+                time++;
+                label1.Text = time.ToString();
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\grass-background-5 - Copie.jpg";
+                if (snakeX == 0) { snakeX = 38; }
+                snakeX = snakeX - 1;
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\snake.jpg";
+            }
+            else if (direction == 1)
+            {
+                time++;
+                label1.Text = time.ToString();
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\grass-background-5 - Copie.jpg";
+                if (snakeX == 37) { snakeX = -1; }
+                snakeX = snakeX + 1;
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\snake.jpg";
+                
+            }
+            else if (direction == 2)
+            {
+                time++;
+                label1.Text = time.ToString();
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\grass-background-5 - Copie.jpg";
+                if (snakeY == 0) { snakeY = 20; }
+                snakeY = snakeY - 1;
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\snake.jpg";
+            }
+            else if (direction == 3)
+            {
+                time++;
+                label1.Text = time.ToString();
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\grass-background-5 - Copie.jpg";
+                if (snakeY == 19) { snakeY = -1; }
+                snakeY = snakeY + 1;
+                pictureBox1[snakeX, snakeY].ImageLocation = @"C:\Users\antoine\Documents\Visual Studio 2015\Projects\snake.jpg";
+            }
+        }
+    }
+}
